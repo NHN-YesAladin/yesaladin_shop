9c6f4b2f32d68950ef40f29398c94b6a5215b807
배수한 <qotngks152@naver.com>
2023-01-05 20:22:31 +0900

    For merge

---
+++ src/main/java/shop/yesaladin/shop/category/persistence/JpaCategoryRepository.java
@@ -0,0 +1,18 @@
+package shop.yesaladin.shop.category.persistence;
+
+import org.springframework.data.repository.Repository;
+import shop.yesaladin.shop.category.domain.model.Category;
+import shop.yesaladin.shop.category.domain.repository.CommandCategoryRepository;
+import shop.yesaladin.shop.category.domain.repository.QueryCategoryRepository;
+
+/**
+ * Category 엔티티를 위한 Jpa repository 구현체
+ *
+ * @author 배수한
+ * @since 1.0
+ */
+
+public interface JpaCategoryRepository extends Repository<Category, Long>,
+        CommandCategoryRepository, QueryCategoryRepository {
+
+}
