99c7a6bae7b51ba8c43837b70c0f011cc86e349c
서민지 <squarearth1@gmail.com>
2023-01-05 22:11:14 +0900

    리뷰/주문상품/결제 엔터티 매핑

---
+++ src/main/java/shop/yesaladin/shop/review/domain/model/ReviewImage.java
@@ -0,0 +1,47 @@
+package shop.yesaladin.shop.review.domain.model;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.OneToOne;
+import javax.persistence.Table;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import shop.yesaladin.shop.review.domain.dummy.File;
+
+/**
+ * 리뷰사진 엔터티
+ *
+ * @author 서민지
+ * @since 1.0
+ */
+@Getter
+@Builder
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor(access = AccessLevel.PRIVATE)
+@Table(name = "review_images")
+@Entity
+public class ReviewImage {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @OneToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "file_id", nullable = false)
+    private File file;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "review_id", nullable = false)
+    private Review review;
+
+    @Column(nullable = false)
+    private int order;
+}
