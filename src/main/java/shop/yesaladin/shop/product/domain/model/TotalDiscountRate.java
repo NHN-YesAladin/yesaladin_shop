2159bee6d8bc76fb04c151cfabe9a6be0df40053
SuJeong68 <tidh1640@naver.com>
2023-01-05 20:23:54 +0900

    Feat: 상품&상품 관련 엔터티 매핑

---
+++ src/main/java/shop/yesaladin/shop/product/domain/model/TotalDiscountRate.java
@@ -0,0 +1,33 @@
+package shop.yesaladin.shop.product.domain.model;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+/**
+ * 전체 할인율의 엔터티 클래스이다.
+ *
+ * @author : 이수정
+ * @since : 1.0
+ */
+@Getter
+@Builder
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor(access = AccessLevel.PRIVATE)
+@Table(name = "total_discount_rates")
+@Entity
+public class TotalDiscountRate {
+
+    @Id
+    private int id;
+
+    @Column(name = "discount_rate", nullable = false)
+    private int discountRate;
+
+}
