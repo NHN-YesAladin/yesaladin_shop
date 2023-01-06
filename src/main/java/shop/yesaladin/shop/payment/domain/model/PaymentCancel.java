2c954e5e07cb4e1d2fcedb012bccefb510a4fee4
서민지 <squarearth1@gmail.com>
2023-01-06 14:54:26 +0900

    Feat: 결제 엔터티 매핑

---
+++ src/main/java/shop/yesaladin/shop/payment/domain/model/PaymentCancel.java
@@ -0,0 +1,58 @@
+package shop.yesaladin.shop.payment.domain.model;
+
+import java.time.LocalDateTime;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.Id;
+import javax.persistence.MapsId;
+import javax.persistence.OneToOne;
+import javax.persistence.Table;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+/**
+ * 결제 카드 정보 엔터티
+ *
+ * @author 서민지
+ * @since 1.0
+ */
+@Getter
+@Builder
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor(access = AccessLevel.PRIVATE)
+@Table(name = "payment_cards")
+@Entity
+public class PaymentCancel {
+    @Id
+    @MapsId
+    @OneToOne(fetch = FetchType.LAZY)
+    private Payment payment;
+
+    @Column(name = "cancel_amount", nullable = false)
+    private long cancelAmount;
+
+    @Column(name = "cancel_reason", nullable = false)
+    private String cancelReason;
+
+    @Column(name = "tax_free_amount", nullable = false)
+    private long taxFreeAmount;
+
+    @Column(name = "tax_exemption_amount", nullable = false)
+    private long taxExemptionAmount;
+
+    @Column(name = "refundable_amount", nullable = false)
+    private long refundableAmount;
+
+    @Column(name = "easy_pay_discount_amount", nullable = false)
+    private long easyPayDiscountAmount;
+
+    @Column(name = "canceled_datetime", nullable = false)
+    private LocalDateTime canceledDatetime;
+
+    @Column(name = "transaction_key", nullable = false)
+    private String transactionKey;
+}
