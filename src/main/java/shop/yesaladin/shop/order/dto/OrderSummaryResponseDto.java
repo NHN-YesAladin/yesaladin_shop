41ba3dfb8ee278f188c421db6e45cd67fe9d469f
Hongdae Kim <mongmeo.dev@gmail.com>
2023-01-07 13:12:55 +0900

    OrderSummaryResponseDto 생성

---
+++ src/main/java/shop/yesaladin/shop/order/dto/OrderSummaryResponseDto.java
@@ -0,0 +1,31 @@
+package shop.yesaladin.shop.order.dto;
+
+import java.time.LocalDateTime;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import shop.yesaladin.shop.order.domain.model.Order;
+import shop.yesaladin.shop.order.domain.model.OrderCode;
+
+/**
+ * 주문 데이터에 대한 대략적인 정보를 가지는 응답용 DTO
+ *
+ * @author 김홍대
+ * @since 1.0
+ */
+@Getter
+@RequiredArgsConstructor(access = AccessLevel.PRIVATE)
+public class OrderSummaryResponseDto {
+
+    private final String orderNumber;
+    private final LocalDateTime orderDateTime;
+    private final OrderCode orderCode;
+
+    public static OrderSummaryResponseDto fromEntity(Order order) {
+        return new OrderSummaryResponseDto(
+                order.getOrderNumber(),
+                order.getOrderDateTime(),
+                order.getOrderCode()
+        );
+    }
+}
