d34af102a8a80da87287e9007363d7d77aeefcd8
SuJeong68 <tidh1640@naver.com>
2023-01-07 13:06:17 +0900

    Feat: 상품 DTO 작성

--- src/main/java/shop/yesaladin/shop/product/controller/ProductController.java
+++ src/main/java/shop/yesaladin/shop/product/controller/ProductController.java
@@ -1,2 +1,24 @@
-package shop.yesaladin.shop.product.controller;public class ProductController {
+package shop.yesaladin.shop.product.controller;
+
+import javax.validation.Valid;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import shop.yesaladin.shop.product.dto.ProductCreateDto;
+
+/**
+ * 상품 등록을 위한 RestController 입니다.
+ *
+ * @author : 이수정
+ * @since : 1.0
+ */
+@RestController
+@RequestMapping("/products")
+public class ProductController {
+
+    @PostMapping
+    public void registerProduct(@Valid @RequestBody ProductCreateDto productCreateDto) {
+
+    }
 }
--- src/main/java/shop/yesaladin/shop/product/dto/ProductCreateDto.java
+++ src/main/java/shop/yesaladin/shop/product/dto/ProductCreateDto.java
@@ -1,2 +1,75 @@
-package shop.yesaladin.shop.product.dto;public class ProductCreateDto {
+package shop.yesaladin.shop.product.dto;
+
+import javax.validation.constraints.Max;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.PositiveOrZero;
+import lombok.Getter;
+import lombok.Setter;
+import org.hibernate.validator.constraints.Length;
+
+/**
+ * 상품 등록을 위한 DTO 입니다.
+ *
+ * @author : 이수정
+ * @since : 1.0
+ */
+@Getter
+@Setter
+public class ProductCreateDto {
+    @NotBlank
+    @Length(max = 50)
+    private String ISBN;
+
+    // 상품 설명
+    @NotBlank
+    @Length(max = 255)
+    private String title;
+    @NotBlank
+    private String contents;
+    @NotBlank
+    private String description;
+
+    // 저자, 출판사
+    @NotBlank
+    private String Writer;
+    @NotBlank
+    private String publisher;
+
+    // 정가, 할인율, 포인트 적립율
+    @PositiveOrZero
+    private long actualPrice;
+    @PositiveOrZero
+    @Max(100)
+    private int discountRate;
+    private Boolean isSeparatelyDiscount;
+    @PositiveOrZero
+    @Max(100)
+    private int givenPointRate;
+    private Boolean isGivenPoint;
+
+    // 구독 상품 관련
+    @NotBlank
+    @Length(max = 9)
+    private String ISSN;
+    private Boolean isSubscriptionAvailable;
+
+    // 판매 여부, 강제품절 여부
+    private Boolean isSale;
+    private Boolean isForcedOutOfStock;
+
+    // 수량, 출간일, 노출우선순위, 썸네일 UUID
+    @PositiveOrZero
+    private long quantity;
+    @NotBlank
+    private String publishedDate;
+    @PositiveOrZero
+    private int preferentialShowRanking;
+    @NotBlank
+    private String uuid;
+
+    // codes
+    @NotBlank
+    private String productTypeCode;
+    @NotBlank
+    private String productSavingMethodCode;
 }
