3dbee30520c48fa4f3930c75981149743ccdd7d0
SuJeong68 <tidh1640@naver.com>
2023-01-10 19:44:57 +0900

    Refactor: CommandProductServiceImplTest를 제외한 테스트 완료

--- src/test/java/shop/yesaladin/shop/product/service/impl/CommandProductServiceImplTest.java
+++ src/test/java/shop/yesaladin/shop/product/service/impl/CommandProductServiceImplTest.java
@@ -2,21 +2,28 @@ package shop.yesaladin.shop.product.service.impl;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyString;
 import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.mockStatic;
 import static org.mockito.Mockito.when;
 
+import java.time.LocalDateTime;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.mockito.MockedStatic;
+import shop.yesaladin.shop.file.domain.model.File;
 import shop.yesaladin.shop.file.service.inter.CommandFileService;
 import shop.yesaladin.shop.file.service.inter.QueryFileService;
 import shop.yesaladin.shop.member.service.inter.QueryMemberService;
 import shop.yesaladin.shop.product.domain.model.Product;
+import shop.yesaladin.shop.product.domain.model.SubscribeProduct;
 import shop.yesaladin.shop.product.domain.model.TotalDiscountRate;
 import shop.yesaladin.shop.product.domain.repository.CommandProductRepository;
 import shop.yesaladin.shop.product.domain.repository.QueryProductRepository;
 import shop.yesaladin.shop.product.dto.ProductCreateDto;
 import shop.yesaladin.shop.product.dto.ProductResponseDto;
 import shop.yesaladin.shop.product.dummy.DummyProductCreateDto;
+import shop.yesaladin.shop.product.dummy.DummyPublisher;
 import shop.yesaladin.shop.product.dummy.DummyTotalDiscountRate;
 import shop.yesaladin.shop.product.service.inter.CommandProductService;
 import shop.yesaladin.shop.product.service.inter.CommandSubscribeProductService;
@@ -24,6 +31,7 @@ import shop.yesaladin.shop.product.service.inter.CommandTotalDiscountRateService
 import shop.yesaladin.shop.product.service.inter.QuerySubscribeProductService;
 import shop.yesaladin.shop.product.service.inter.QueryTotalDiscountRateService;
 import shop.yesaladin.shop.publish.domain.model.Publish;
+import shop.yesaladin.shop.publish.domain.model.Publisher;
 import shop.yesaladin.shop.publish.service.inter.CommandPublishService;
 import shop.yesaladin.shop.publish.service.inter.CommandPublisherService;
 import shop.yesaladin.shop.publish.service.inter.QueryPublisherService;
@@ -68,6 +76,9 @@ class CommandProductServiceImplTest {
     private QueryTagService queryTagService;
     private CommandProductTagService commandProductTagService;
 
+    // TotalDiscountRate
+    private CommandTotalDiscountRateService commandTotalDiscountRateService;
+
     @BeforeEach
     void setUp() {
         commandProductRepository = mock(CommandProductRepository.class);
@@ -85,6 +96,8 @@ class CommandProductServiceImplTest {
         commandTagService = mock(CommandTagService.class);
         queryTagService = mock(QueryTagService.class);
         commandProductTagService = mock(CommandProductTagService.class);
+
+        commandTotalDiscountRateService = mock(CommandTotalDiscountRateService.class);
 
         commandProductService = new CommandProductServiceImpl(
                 commandProductRepository,
@@ -107,19 +120,37 @@ class CommandProductServiceImplTest {
 
     @Test
     void create() {
-        // TODO: 수정 필요!!
-
         // given
         ProductCreateDto productCreateDto = DummyProductCreateDto.dummy(ISBN);
+
+        SubscribeProduct subscribeProduct = productCreateDto.toSubscribeProductEntity();
+        File thumbnailFile = productCreateDto.toThumbnailFileEntity();
+        File ebookFile = productCreateDto.toEbookFileEntity();
+        TotalDiscountRate totalDiscountRate = DummyTotalDiscountRate.dummy();
+
+        commandSubscribeProductService.register(subscribeProduct);
+        commandFileService.register(thumbnailFile);
+        commandFileService.register(ebookFile);
+        commandTotalDiscountRateService.register(totalDiscountRate);
+
         Product product = productCreateDto.toProductEntity(
-                productCreateDto.toSubscribeProductEntity(),
-                productCreateDto.toThumbnailFileEntity(),
-                productCreateDto.toEbookFileEntity(),
-                DummyTotalDiscountRate.dummy()
+                subscribeProduct,
+                thumbnailFile,
+                ebookFile,
+                totalDiscountRate
         );
 
         when(commandProductRepository.save(any())).thenReturn(product);
 
+        Publisher publisher = DummyPublisher.dummy();
+        commandPublisherService.register(publisher);
+
+        // TODO: test......모르겠어요... 다시해요...
+//        mockStatic(Publish.class);
+
+        String now = LocalDateTime.now().toLocalDate().toString();
+        Publish publish = Publish.create(product, publisher, now);
+        when(commandPublishService.register(any())).thenReturn(publish);
 
         // when
         ProductResponseDto productResponseDto = commandProductService.create(productCreateDto);
