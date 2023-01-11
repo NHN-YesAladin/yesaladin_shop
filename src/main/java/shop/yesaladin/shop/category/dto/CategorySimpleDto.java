fc872822f4bb9177db00d0cdf7dc1696b91a1ef1
배수한 <qotngks152@naver.com>
2023-01-12 01:58:40 +0900

    Docs: javadoc 보강

--- src/main/java/shop/yesaladin/shop/category/dto/CategorySimpleDto.java
+++ src/main/java/shop/yesaladin/shop/category/dto/CategorySimpleDto.java
@@ -6,6 +6,8 @@ import lombok.NoArgsConstructor;
 import lombok.ToString;
 
 /**
+ * 카테고리에 대한 기본 정보 아이디, 이름, 노출여부, 순서 에 대한 정보를 담고있는 dto
+ *
  * @author 배수한
  * @since 1.0
  */
--- src/main/java/shop/yesaladin/shop/category/persistence/QueryDslCategoryRepositoryImpl.java
+++ src/main/java/shop/yesaladin/shop/category/persistence/QueryDslCategoryRepositoryImpl.java
@@ -31,6 +31,12 @@ public class QueryDslCategoryRepositoryImpl implements QueryCategoryRepository {
 
     private final JPAQueryFactory queryFactory;
 
+    /**
+     * 카테고리 paging list 조회
+     *
+     * @param pageable size 와 page 를 가진 객체
+     * @return paging 되어있는 Category Page 객체
+     */
     @Override
     public Page<Category> findAll(Pageable pageable) {
         QCategory category = QCategory.category;
@@ -42,6 +48,12 @@ public class QueryDslCategoryRepositoryImpl implements QueryCategoryRepository {
         return new PageImpl<>(categories, pageable, count);
     }
 
+    /**
+     * id를 통한 카테고리 조회
+     *
+     * @param id 카테고리 id
+     * @return Optional 처리가 된 카테고리 객체
+     */
     @Override
     public Optional<Category> findById(Long id) {
         QCategory category = QCategory.category;
@@ -56,6 +68,12 @@ public class QueryDslCategoryRepositoryImpl implements QueryCategoryRepository {
         return categoryOptional;
     }
 
+    /**
+     * 카테고리 이름을 통한 카테고리 조회
+     *
+     * @param name 카테고리 이름
+     * @return Optional 처리가 된 카테고리 객체
+     */
     @Override
     public Optional<Category> findByName(String name) {
         QCategory category = QCategory.category;
@@ -70,6 +88,14 @@ public class QueryDslCategoryRepositoryImpl implements QueryCategoryRepository {
         return categoryOptional;
     }
 
+    /**
+     * 카테고리 id의 마지막 값을 depth와 부모 id를 통해 조회
+     *   2차 카테고리의 마지막 id를 찾아오기 위해 사용
+     *
+     * @param depth 2차 카테고리의 깊이 값인 1이 입력됨
+     * @param parentId 2차 카테고리가 가지고있는 부모 id
+     * @return Long id 만 가지고있음
+     */
     @Override
     public CategoryOnlyIdDto getLatestChildIdByDepthAndParentId(int depth, Long parentId) {
         QCategory category = QCategory.category;
@@ -84,6 +110,13 @@ public class QueryDslCategoryRepositoryImpl implements QueryCategoryRepository {
         return new CategoryOnlyIdDto(fetchOne);
     }
 
+    /**
+     * 카테고리의 id의 마지막 값을 depth를 통해 조회
+     *   1차 카테고리의 마지막 id를 찾아오기 위해 사용
+     *
+     * @param depth 1차 카테고리의 깊이 값인 0이 입력됨
+     * @return Long id 만 가지고있음
+     */
     @Override
     public CategoryOnlyIdDto getLatestIdByDepth(int depth) {
         QCategory category = QCategory.category;
@@ -99,6 +132,12 @@ public class QueryDslCategoryRepositoryImpl implements QueryCategoryRepository {
         return new CategoryOnlyIdDto(fetchOne);
     }
 
+    /**
+     * parentId 를 통해서 DtoProjection 으로 데이터를 선별하여 return 하는 기능
+     *
+     * @param parentId 찾고자하는 카테고리의 parentId
+     * @return CategorySimpleDto 카테고리의 기본 정보를 담고있는 dto
+     */
     @Override
     public List<CategorySimpleDto> findSimpleDtosByParentId(Long parentId) {
         QCategory category = QCategory.category;
@@ -115,6 +154,12 @@ public class QueryDslCategoryRepositoryImpl implements QueryCategoryRepository {
         return simpleDtoList;
     }
 
+    /**
+     * depth를 통해서 카테고리를 조회하여 카테고리의 기본 정보를 담고있는 dto를 return 하는 기능
+     *
+     * @param depth 1차는 '0' , 2차는 '1'
+     * @return CategorySimpleDto 카테고리의 기본 정보를 담고있는 dto
+     */
     @Override
     public List<CategorySimpleDto> findSimpleDtosByDepth(int depth) {
         QCategory category = QCategory.category;
@@ -129,6 +174,12 @@ public class QueryDslCategoryRepositoryImpl implements QueryCategoryRepository {
                 .fetch();
     }
 
+    /**
+     * id 를 통해 카테고리를 조회 할 경우, N+1을 해결 하기 위해 fetch join 실행
+     *
+     * @param id 찾고자하는 카테고리 id
+     * @return Optional<Category>
+     */
     @Override
     public Optional<Category> findByIdByFetching(Long id) {
         QCategory category = QCategory.category;
