b81f76a8af31647efea9feb4eb3d0ee81b3ff7a2
Hongdae Kim <mongmeo.dev@gmail.com>
2023-01-12 14:43:54 +0900

    Feat: PeriodQueryRequestDto에 validate logic 추가

--- src/main/java/shop/yesaladin/shop/member/service/impl/QueryMemberGradeHistoryServiceImpl.java
+++ src/main/java/shop/yesaladin/shop/member/service/impl/QueryMemberGradeHistoryServiceImpl.java
@@ -1,14 +1,11 @@
 package shop.yesaladin.shop.member.service.impl;
 
 import java.time.Clock;
-import java.time.LocalDate;
 import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import shop.yesaladin.shop.common.dto.PeriodQueryRequestDto;
-import shop.yesaladin.shop.common.exception.InvalidPeriodConditionException;
-import shop.yesaladin.shop.common.exception.type.InvalidPeriodConditionType;
 import shop.yesaladin.shop.member.domain.repository.QueryMemberGradeHistoryRepository;
 import shop.yesaladin.shop.member.dto.MemberGradeHistoryQueryResponseDto;
 import shop.yesaladin.shop.member.service.inter.QueryMemberGradeHistoryService;
@@ -32,21 +29,11 @@ public class QueryMemberGradeHistoryServiceImpl implements QueryMemberGradeHisto
             long memberId,
             PeriodQueryRequestDto request
     ) {
-        checkValidQueryCondition(request);
+        request.validate(clock);
         return queryMemberGradeHistoryRepository.findByMemberIdAndPeriod(
                 memberId,
                 request.getStartDateOrDefaultValue(clock),
                 request.getEndDateOrDefaultValue(clock)
         );
-    }
-
-    private void checkValidQueryCondition(PeriodQueryRequestDto queryDto) {
-        if (queryDto.getStartDateOrDefaultValue(clock)
-                .isAfter(queryDto.getEndDateOrDefaultValue(clock))) {
-            throw new InvalidPeriodConditionException(InvalidPeriodConditionType.START_OVER_END);
-        }
-        if (queryDto.getEndDateOrDefaultValue(clock).isAfter(LocalDate.now(clock))) {
-            throw new InvalidPeriodConditionException(InvalidPeriodConditionType.FUTURE);
-        }
     }
 }
