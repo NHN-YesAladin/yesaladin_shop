8a0f328359da5f45ec1c38834a3c955fe6e66d3d
최예린 <yerin6860@gmail.com>
2023-01-19 16:28:33 +0900

    Feat: error response dto 추가

---
+++ src/main/java/shop/yesaladin/shop/common/dto/ErrorResponseDto.java
@@ -0,0 +1,17 @@
+package shop.yesaladin.shop.common.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+/**
+ * 에러 메세지 반환 dto 클래스입니다.
+ *
+ * @author 최예린
+ * @since 1.0
+ */
+@Getter
+@RequiredArgsConstructor
+public class ErrorResponseDto {
+    private final String message;
+}
