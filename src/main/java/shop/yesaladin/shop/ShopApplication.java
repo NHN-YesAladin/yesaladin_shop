b7ca12b301b72e17fb7b5e5cea393c67d7c97911
Hongdae Kim <mongmeo.dev@gmail.com>
2023-01-19 13:18:01 +0900

    [Feat] : 불필요한 의존성 삭제 및 Coupon-Utils 의존성 추가

--- src/main/java/shop/yesaladin/shop/ShopApplication.java
+++ src/main/java/shop/yesaladin/shop/ShopApplication.java
@@ -2,9 +2,7 @@ package shop.yesaladin.shop;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
-@EnableEurekaClient
 @SpringBootApplication
 public class ShopApplication {
 
