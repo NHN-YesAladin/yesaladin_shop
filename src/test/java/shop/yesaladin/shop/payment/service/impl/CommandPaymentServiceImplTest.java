246cf0061e787253d099fc5a76df76764445a7b7
배수한 <qotngks152@naver.com>
2023-01-20 14:36:55 +0900

    Feat: getOrderByName 서비스 메서드 추가 및 테스트 완료

---
+++ src/test/java/shop/yesaladin/shop/payment/service/impl/CommandPaymentServiceImplTest.java
@@ -0,0 +1,21 @@
+package shop.yesaladin.shop.payment.service.impl;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+/**
+ * @author 배수한
+ * @since 1.0
+ */
+class CommandPaymentServiceImplTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @Test
+    void confirmTossRequest() {
+    }
+}
