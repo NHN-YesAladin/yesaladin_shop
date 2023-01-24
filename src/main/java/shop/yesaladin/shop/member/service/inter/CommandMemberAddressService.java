ec3dcc1f34ffcf28e82f3cefa22d4b0740c0afd4
최예린 <yerin6860@gmail.com>
2023-01-24 19:37:31 +0900

    Fix: 회원 배송지 관련 api 수정

--- src/main/java/shop/yesaladin/shop/member/service/inter/CommandMemberAddressService.java
+++ src/main/java/shop/yesaladin/shop/member/service/inter/CommandMemberAddressService.java
@@ -15,33 +15,33 @@ public interface CommandMemberAddressService {
     /**
      * 회원의 배송지를 등록합니다.
      *
-     * @param memberId 회원 id
+     * @param loginId 회원의 아이디
      * @param request  등록할 배송지 데이터
      * @return 등록된 배송지 데이터
      * @author 최예린
      * @since 1.0
      */
-    MemberAddressCreateResponseDto save(long memberId, MemberAddressCreateRequestDto request);
+    MemberAddressCreateResponseDto save(String loginId, MemberAddressCreateRequestDto request);
 
     /**
      * 회원의 대표 배송지를 등록합니다.
      *
-     * @param memberId  회원 id
+     * @param loginId  회원의 아이디
      * @param addressId 대표 배송지 아이디
      * @return 대표 배송지 데이터
      * @author 최예린
      * @since 1.0
      */
-    MemberAddressUpdateResponseDto markAsDefault(long memberId, long addressId);
+    MemberAddressUpdateResponseDto markAsDefault(String loginId, long addressId);
 
     /**
      * 회원의 배송지를 삭제합니다.
      *
-     * @param memberId  회원 id
+     * @param loginId  회원의 아이디
      * @param addressId 삭제할 배송지 id
-     * @return
+     * @return 삭제된 회원의 배송지 id
      * @author 최예린
      * @since 1.0
      */
-    long delete(long memberId, long addressId);
+    long delete(String loginId, long addressId);
 }
