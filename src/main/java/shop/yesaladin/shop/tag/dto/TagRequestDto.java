93b9e6ad7837baf314f12e22363bae97a4f06f2c
SuJeong68 <tidh1640@naver.com>
2023-01-30 01:56:12 +0900

    머지용 커밋

---
+++ src/main/java/shop/yesaladin/shop/tag/dto/TagRequestDto.java
@@ -0,0 +1,24 @@
+package shop.yesaladin.shop.tag.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.validation.constraints.NotBlank;
+
+/**
+ * 태그 등록을 위한 Dto 입니다.
+ *
+ * @author 이수정
+ * @since 1.0
+ */
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class TagRequestDto {
+
+    @NotBlank
+    private String name;
+}
