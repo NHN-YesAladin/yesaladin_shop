95915e2218bfefc0a30e97db47854b1b12e1a87a
SuJeong68 <tidh1640@naver.com>
2023-02-02 17:24:19 +0900

    중간 커밋

--- src/main/java/shop/yesaladin/shop/product/dto/ProductDetailResponseDto.java
+++ src/main/java/shop/yesaladin/shop/product/dto/ProductDetailResponseDto.java
@@ -35,7 +35,7 @@ public class ProductDetailResponseDto {
 
     private String publishedDate;
     private String isbn;
-    private boolean isSubscriptionAvailable;
+    private Boolean isSubscriptionAvailable;
     private String issn;
 
     private String contents;
@@ -44,7 +44,7 @@ public class ProductDetailResponseDto {
 
     // 판매여부 관련
     private long quantity;
-    private boolean isForcedOutOfStock;
-    private boolean isSale;
-    private boolean isDeleted;
+    private Boolean isForcedOutOfStock;
+    private Boolean isSale;
+    private Boolean isDeleted;
 }
