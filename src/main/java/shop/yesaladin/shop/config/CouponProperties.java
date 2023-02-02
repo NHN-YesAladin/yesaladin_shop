f2c4512fdca023db004bc7e27d5b6297c3c69795
Hongdae Kim <mongmeo.dev@gmail.com>
2023-02-03 08:19:03 +0900

    [Feat] : CouponProperties.java 추가

---
+++ src/main/java/shop/yesaladin/shop/config/CouponProperties.java
@@ -0,0 +1,22 @@
+package shop.yesaladin.shop.config;
+
+import lombok.Getter;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * 쿠폰 관련 설정 값을 가져오기 위한 클래스입니다.
+ *
+ * @author 김홍대
+ * @since 1.0
+ */
+@Getter
+@Configuration
+public class CouponProperties {
+
+    @Value("${coupon.topic.user-download}")
+    private String userDownloadCouponTopic;
+
+    @Value("${coupon.topic.auto-issuance}")
+    private String autoIssuanceCouponTopic;
+}
