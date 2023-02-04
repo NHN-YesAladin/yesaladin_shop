33a68e4694a01505f837d88138e1a2f75ed10df6
Hongdae Kim <mongmeo.dev@gmail.com>
2023-02-04 23:58:45 +0900

    [Feat] : CouponGroupAndLimitDto에 AllArgConstructor 추가

--- src/main/java/shop/yesaladin/shop/coupon/dto/CouponGroupAndLimitDto.java
+++ src/main/java/shop/yesaladin/shop/coupon/dto/CouponGroupAndLimitDto.java
@@ -1,11 +1,13 @@
 package shop.yesaladin.shop.coupon.dto;
 
 import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 @Getter
 @NoArgsConstructor(access = AccessLevel.PRIVATE)
+@AllArgsConstructor
 public class CouponGroupAndLimitDto {
 
     private String couponGroupCode;
