93bfa87197879912410b10e1d8605b516177962d
Hongdae Kim <mongmeo.dev@gmail.com>
2023-02-04 19:38:47 +0900

    [Refactor] Properties에서 Configuration 클래스로 카프카 설정 이동

---
+++ src/main/java/shop/yesaladin/shop/config/KafkaConsumerConfiguration.java
@@ -0,0 +1,43 @@
+package shop.yesaladin.shop.config;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import org.apache.kafka.clients.consumer.ConsumerConfig;
+import org.apache.kafka.clients.consumer.CooperativeStickyAssignor;
+import org.apache.kafka.common.serialization.StringDeserializer;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.kafka.core.ConsumerFactory;
+import org.springframework.kafka.core.DefaultKafkaConsumerFactory;
+import org.springframework.kafka.support.serializer.JsonDeserializer;
+
+@Configuration
+public class KafkaConsumerConfiguration {
+
+    @Value("${spring.kafka.bootstrap-servers}")
+    private List<String> bootstrapServers;
+
+    @Bean
+    public ConsumerFactory<String, Object> consumerFactory() {
+        return new DefaultKafkaConsumerFactory<>(consumerConfig());
+    }
+
+    @Bean
+    public Map<String, Object> consumerConfig() {
+        Map<String, Object> props = new HashMap<>();
+        props.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapServers);
+        props.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
+        props.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, JsonDeserializer.class);
+        props.put(
+                ConsumerConfig.PARTITION_ASSIGNMENT_STRATEGY_CONFIG,
+                List.of(CooperativeStickyAssignor.class)
+        );
+        props.put(JsonDeserializer.TRUSTED_PACKAGES, "shop.yesaladin.*");
+        props.put(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG, "earliest");
+        props.put(JsonDeserializer.VALUE_DEFAULT_TYPE, HashMap.class);
+        return props;
+    }
+
+}
