02b7c786abc639b7fc4dfcbc847c13f66bfbcba6
Hongdae Kim <mongmeo.dev@gmail.com>
2023-02-04 20:08:29 +0900

    [Feat] : KafkaListenerContainerFactory Bean 생성

--- src/main/java/shop/yesaladin/shop/config/KafkaConsumerConfiguration.java
+++ src/main/java/shop/yesaladin/shop/config/KafkaConsumerConfiguration.java
@@ -9,15 +9,29 @@ import org.apache.kafka.common.serialization.StringDeserializer;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.kafka.annotation.EnableKafka;
+import org.springframework.kafka.config.ConcurrentKafkaListenerContainerFactory;
+import org.springframework.kafka.config.KafkaListenerContainerFactory;
 import org.springframework.kafka.core.ConsumerFactory;
 import org.springframework.kafka.core.DefaultKafkaConsumerFactory;
+import org.springframework.kafka.listener.ConcurrentMessageListenerContainer;
 import org.springframework.kafka.support.serializer.JsonDeserializer;
 
+@EnableKafka
 @Configuration
 public class KafkaConsumerConfiguration {
 
     @Value("${spring.kafka.bootstrap-servers}")
     private List<String> bootstrapServers;
+
+    @Bean
+    public KafkaListenerContainerFactory<ConcurrentMessageListenerContainer<String, Object>> kafkaListenerContainerFactory() {
+        ConcurrentKafkaListenerContainerFactory<String, Object> factory =
+                new ConcurrentKafkaListenerContainerFactory<>();
+        factory.setConsumerFactory(consumerFactory());
+        factory.setConcurrency(3);
+        return factory;
+    }
 
     @Bean
     public ConsumerFactory<String, Object> consumerFactory() {
