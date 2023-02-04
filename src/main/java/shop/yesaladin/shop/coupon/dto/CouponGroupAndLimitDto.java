a8f370bda8c63efe710bd8f80b9d1ae8e84d3284
Hongdae Kim <mongmeo.dev@gmail.com>
2023-02-04 20:49:15 +0900

    [Feat] : CouponGroupCodesAndLimitDto 수정

--- src/main/java/shop/yesaladin/shop/coupon/dto/CouponGroupAndLimitDto.java
+++ src/main/java/shop/yesaladin/shop/coupon/dto/CouponGroupAndLimitDto.java
@@ -1,14 +1,13 @@
 package shop.yesaladin.shop.coupon.dto;
 
-import java.util.List;
 import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 @Getter
 @NoArgsConstructor(access = AccessLevel.PRIVATE)
-public class CouponGroupCodesAndLimitDto {
+public class CouponGroupAndLimitDto {
 
-    private List<String> couponCodes;
+    private String couponGroupCode;
     private Boolean isLimited;
 }
