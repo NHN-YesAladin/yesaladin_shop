9244462cdecc605beb9b5c09f06247eb0575b4fe
Hongdae Kim <mongmeo.dev@gmail.com>
2023-02-06 17:03:11 +0900

    [Fix] : MemberCouponSummaryDto에 기본 생성자 추가

--- src/main/java/shop/yesaladin/shop/coupon/dto/MemberCouponSummaryDto.java
+++ src/main/java/shop/yesaladin/shop/coupon/dto/MemberCouponSummaryDto.java
@@ -4,11 +4,13 @@ import java.time.LocalDate;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import shop.yesaladin.coupon.code.CouponBoundCode;
 import shop.yesaladin.coupon.code.CouponTypeCode;
 
 @Getter
 @Builder
+@NoArgsConstructor
 @AllArgsConstructor
 public class MemberCouponSummaryDto {
 
