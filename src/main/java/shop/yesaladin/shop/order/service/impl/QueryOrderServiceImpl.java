0d696eea8fdb53b46adb853864a8164ac1dd795c
배수한 <qotngks152@naver.com>
2023-02-08 21:20:35 +0900

    Feat: 트랜잭셔널 어노테이션 추가

--- src/main/java/shop/yesaladin/shop/order/service/impl/QueryOrderServiceImpl.java
+++ src/main/java/shop/yesaladin/shop/order/service/impl/QueryOrderServiceImpl.java
@@ -180,6 +180,7 @@ public class QueryOrderServiceImpl implements QueryOrderService {
     }
 
     @Override
+    @Transactional(readOnly = true)
     public Page<OrderStatusResponseDto> getStatusResponsesByLoginIdAndStatus(
             String loginId,
             OrderStatusCode code,
