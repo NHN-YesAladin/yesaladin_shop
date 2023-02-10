c035bb0eb14abb000315aea98e5dccd9a5ede379
배수한 <qotngks152@naver.com>
2023-02-10 16:39:23 +0900

    Feat: delivery 관련 event 서비스 구현 - 테스트 필요

---
+++ src/main/java/shop/yesaladin/shop/delivery/dto/TransportResponseDto.java
@@ -0,0 +1,26 @@
+package shop.yesaladin.shop.delivery.dto;
+
+import java.time.LocalDate;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+/**
+ * 배송 정보를 담은 DTO 클래스 입니다.
+ *
+ * @author 배수한
+ * @since 1.0
+ */
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+public class TransportResponseDto {
+
+    private Long id;
+    private LocalDate receptionDatetime;
+    private LocalDate completionDatetime;
+    private Long orderId;
+    private String trackingNo;
+    private String transportStatus;
+}
