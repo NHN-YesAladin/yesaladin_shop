78347f869e8c709c0175903391059bce47079ba2
Hongdae Kim <mongmeo.dev@gmail.com>
2023-02-12 15:57:13 +0900

    [Docs] : 자바독 추가

--- src/main/java/shop/yesaladin/shop/coupon/dto/CouponGiveRequestDto.java
+++ src/main/java/shop/yesaladin/shop/coupon/dto/CouponGiveRequestDto.java
@@ -6,6 +6,12 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import shop.yesaladin.coupon.code.TriggerTypeCode;
 
+/**
+ * 쿠폰 지급 요청시 사용하는 DTO 클래스입니다.
+ *
+ * @author 김홍대
+ * @since 1.0
+ */
 @Getter
 @NoArgsConstructor(access = AccessLevel.PRIVATE)
 @AllArgsConstructor
