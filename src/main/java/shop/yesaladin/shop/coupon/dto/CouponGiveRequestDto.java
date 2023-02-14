b011acf643922f93d45198e1ea1c3a97f89f5bd8
서민지 <squarearth1@gmail.com>
2023-02-14 17:30:16 +0900

    [Feat]: 쿠폰발행요청dto 의 요청 시간 dateTimeFormat 변경

--- src/main/java/shop/yesaladin/shop/coupon/dto/CouponGiveRequestDto.java
+++ src/main/java/shop/yesaladin/shop/coupon/dto/CouponGiveRequestDto.java
@@ -6,6 +6,7 @@ import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import org.springframework.format.annotation.DateTimeFormat;
+import org.springframework.format.annotation.DateTimeFormat.ISO;
 import shop.yesaladin.coupon.code.TriggerTypeCode;
 
 /**
@@ -21,6 +22,6 @@ public class CouponGiveRequestDto {
 
     private TriggerTypeCode triggerTypeCode;
     private Long couponId;
-    @DateTimeFormat(pattern = "yy- MM-dd HH:mm")
+    @DateTimeFormat(iso = ISO.DATE_TIME)
     private LocalDateTime requestDateTime;
 }
