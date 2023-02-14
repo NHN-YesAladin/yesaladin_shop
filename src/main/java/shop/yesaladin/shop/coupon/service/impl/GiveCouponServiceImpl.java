67ae954d39a58ff40f3dd33a2d3481569358b099
Hongdae Kim <mongmeo.dev@gmail.com>
2023-02-14 17:38:46 +0900

    [Docs] : 주석 수정

--- src/main/java/shop/yesaladin/shop/coupon/service/impl/GiveCouponServiceImpl.java
+++ src/main/java/shop/yesaladin/shop/coupon/service/impl/GiveCouponServiceImpl.java
@@ -82,7 +82,7 @@ public class GiveCouponServiceImpl implements GiveCouponService {
             checkMonthlyCouponIssueRequestTime(requestDateTime);
         }
 
-        if (Objects.nonNull(couponId)) {    // 자동 발행 타입 쿠폰을 요청하는 경우
+        if (Objects.nonNull(couponId)) {    // 수동 발행 타입 쿠폰을 요청하는 경우
             registerIssueRequest(memberId, triggerTypeCode.name(), couponId.toString());
         }
         List<CouponGroupAndLimitDto> couponGroupAndLimitList = getCouponGroupAndLimit(
