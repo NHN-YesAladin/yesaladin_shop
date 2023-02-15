08e68cacfb846362a208841e9d65cfd7560af153
서민지 <squarearth1@gmail.com>
2023-02-15 22:15:23 +0900

    [Feat]: UseCouponController 테스트, 포인트 쿠폰 사용 테스트 작성

--- src/main/java/shop/yesaladin/shop/coupon/dto/CouponUseRequestDto.java
+++ src/main/java/shop/yesaladin/shop/coupon/dto/CouponUseRequestDto.java
@@ -3,8 +3,10 @@ package shop.yesaladin.shop.coupon.dto;
 import java.util.List;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 @Getter
+@NoArgsConstructor
 @AllArgsConstructor
 public class CouponUseRequestDto {
 
