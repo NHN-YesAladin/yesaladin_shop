dbab290f20d6d636846558c9d92b9fe9fa22d1ec
SuJeong68 <tidh1640@naver.com>
2023-02-16 17:34:42 +0900

    Fix: 불필요 컨피그 수정

--- src/main/java/shop/yesaladin/shop/config/WebMvcConfig.java
+++ src/main/java/shop/yesaladin/shop/config/WebMvcConfig.java
@@ -1,7 +1,6 @@
 package shop.yesaladin.shop.config;
 
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import shop.yesaladin.shop.interceptor.RequestLoggingInterceptor;
@@ -12,12 +11,5 @@ public class WebMvcConfig implements WebMvcConfigurer {
     @Override
     public void addInterceptors(InterceptorRegistry registry) {
         registry.addInterceptor(new RequestLoggingInterceptor());
-    }
-
-    @Override
-    public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/v1/products/**")
-                .allowedOrigins("http://localhost:9090", "https://www.yesaladin.shop", "https://test.yesaladin.shop")
-                .allowedMethods("GET", "POST", "PUT");
     }
 }
