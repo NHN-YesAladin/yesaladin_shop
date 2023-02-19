190f8a476b3e88abcf8dc2f35f8ec420da29e0be
배수한 <qotngks152@naver.com>
2023-02-20 00:15:29 +0900

    Feat: 비회원 주문 조회시, 회원 주문 일 경우 forbidden 예외 처리

--- src/main/java/shop/yesaladin/shop/order/controller/QueryOrderController.java
+++ src/main/java/shop/yesaladin/shop/order/controller/QueryOrderController.java
@@ -13,7 +13,9 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import shop.yesaladin.common.code.ErrorCode;
 import shop.yesaladin.common.dto.ResponseDto;
+import shop.yesaladin.common.exception.ClientException;
 import shop.yesaladin.shop.common.aspect.annotation.LoginId;
 import shop.yesaladin.shop.common.dto.PaginatedResponseDto;
 import shop.yesaladin.shop.common.dto.PeriodQueryRequestDto;
@@ -25,7 +27,6 @@ import shop.yesaladin.shop.order.dto.OrderSummaryDto;
 import shop.yesaladin.shop.order.dto.SalesStatisticsResponseDto;
 import shop.yesaladin.shop.order.service.inter.QueryOrderService;
 import java.util.List;
-import java.util.Objects;
 
 /**
  * 전체 주문 조회 관련 rest controller 클래스 입니다.
@@ -98,7 +99,18 @@ public class QueryOrderController {
      * @return 주문 상세 조회 관련 정보
      */
     @GetMapping("/v1/orders/{orderNumber}")
-    public ResponseDto<OrderDetailsResponseDto> getOrderDetails(@PathVariable String orderNumber) {
+    public ResponseDto<OrderDetailsResponseDto> getOrderDetails(
+            @PathVariable String orderNumber,
+            @RequestParam(value = "type", required = false) String type
+    ) {
+        // 비회원 주문 번호 조회로 회원 주문번호 조회했을 경우, 권한 없음 예외 발생
+        if (Objects.equals(type, "none") && queryOrderService.isMemberOrder(orderNumber)) {
+            throw new ClientException(
+                    ErrorCode.FORBIDDEN,
+                    ErrorCode.FORBIDDEN.getDisplayName() + " 회원 주문번호는 비회원 주문 조회에서는 조회가 불가합니다."
+            );
+        }
+
         OrderDetailsResponseDto response = queryOrderService.getDetailsDtoByOrderNumber(
                 orderNumber);
 
