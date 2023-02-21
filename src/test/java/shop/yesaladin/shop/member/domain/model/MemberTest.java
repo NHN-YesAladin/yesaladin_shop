05cf352d607082e5d0352253e64d026a70847001
alanhakhyeonsong <songs4805@naver.com>
2023-02-21 12:41:03 +0900

    Test: member entity unit test 추가

--- src/test/java/shop/yesaladin/shop/member/domain/model/MemberTest.java
+++ src/test/java/shop/yesaladin/shop/member/domain/model/MemberTest.java
@@ -68,4 +68,82 @@ class MemberTest {
         //then
         assertThat(member.getNickname()).isEqualTo(newNickname);
     }
+
+    @Test
+    void isSameLoginId() throws Exception {
+        //given
+        Member member = Member.builder().loginId("test").build();
+        Member compare = Member.builder().loginId("test").build();
+
+        //when
+        boolean result = member.isSameLoginId(compare);
+
+        //then
+        assertThat(result).isTrue();
+    }
+
+    @Test
+    void isSameNickname() throws Exception {
+        //given
+        Member member = Member.builder().nickname("test").build();
+        Member compare = Member.builder().nickname("test").build();
+
+        //when
+        boolean result = member.isSameNickname(compare);
+
+        //then
+        assertThat(result).isTrue();
+    }
+
+    @Test
+    void changeName() {
+        //given
+        String newName = "new";
+        Member member = Member.builder().name("origin").build();
+
+        //when
+        member.changeName(newName);
+
+        //then
+        assertThat(member.getName()).isEqualTo(newName);
+    }
+
+    @Test
+    void changeEmail() throws Exception {
+        //given
+        String newEmail = "test@test.com";
+        Member member = Member.builder().email("origin").build();
+
+        //when
+        member.changeEmail(newEmail);
+
+        //then
+        assertThat(member.getEmail()).isEqualTo(newEmail);
+    }
+
+    @Test
+    void changePhone() throws Exception {
+        //given
+        String newPhone = "01012345678";
+        Member member = Member.builder().phone("origin").build();
+
+        //when
+        member.changePhone(newPhone);
+
+        //then
+        assertThat(member.getPhone()).isEqualTo(newPhone);
+    }
+
+    @Test
+    void changePassword() throws Exception {
+        //given
+        String newPassword = "test";
+        Member member = Member.builder().password("origin").build();
+
+        //when
+        member.changePassword(newPassword);
+
+        //then
+        assertThat(member.getPassword()).isEqualTo(newPassword);
+    }
 }
